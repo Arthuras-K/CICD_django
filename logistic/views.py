e2d670b8aef4316807a8eee373d0f52c0cd63770
Arthuras <a-r-ch@yandex.ru>
2023-02-11 01:02:58 +0300

    fix3

--- logistic/views.py
+++ logistic/views.py
@@ -1,7 +1,8 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
-
 from .models import Stock, Product
 from .serializers import ProductSerializer, StockSerializer
 
@@ -15,7 +16,7 @@ class ProductViewSet(ModelViewSet):
     @action(['GET'], detail=False)
     def test(self, request):
         return Response('HELLO WORLD!')
-        
+
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
